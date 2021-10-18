d6f17140c795705c0efbac4193abbe74f38f64c8
newmanrs <newmanrs@umich.edu>
2021-10-17 18:12:02 -0700

    formatting changes

--- config_neo4j_indexes.py
+++ config_neo4j_indexes.py
@@ -2,14 +2,16 @@ from neo4j import GraphDatabase
 import json
 import os
 
+
 def create_indexes(session):
 
     swt = session.write_transaction
 
-    propindex = {'Beer':'name',
-        'Hop':'name',
-        'Style':'style',
-        'Aroma':'aroma',
+    propindex = {
+        'Beer': 'name',
+        'Hop': 'name',
+        'Style': 'style',
+        'Aroma': 'aroma',
         }
 
     for label, prop in propindex.items():
@@ -21,19 +23,20 @@ def create_indexes(session):
 
     # Fun fact - Neo4j's query to check indexes is
     # rejected if run as a read transaction. Mkay.
-    if not swt(index_exists,index_name):
-        swt(create_fulltext_index,index_name, nodes, properties)
+    if not swt(index_exists, index_name):
+        swt(create_fulltext_index, index_name, nodes, properties)
 
     swt(print_db_indexes)
 
 
-def create_property_index(tx,label,prop):
+def create_property_index(tx, label, prop):
     query = """
         CREATE INDEX {} IF NOT EXISTS FOR (n:{}) ON (n.{})
         """.format(label+'_'+prop, label, prop)
     tx.run(query)
 
-def index_exists(tx,index_name):
+
+def index_exists(tx, index_name):
     """ Check for index existence to prevent throwing
     exceptions in the code.
     """
@@ -45,29 +48,31 @@ def index_exists(tx,index_name):
     return False
 
 
-def create_fulltext_index(tx,name,nodes,properties):
+def create_fulltext_index(tx, name, nodes, properties):
 
     name = 'name_and_descriptions'
-    nodes = ['Beer','Hop']
+    nodes = ['Beer', 'Hop']
     properties = ['name', 'description']
 
     query = """
         CALL db.index.fulltext.createNodeIndex('{}', {}, {})
-        """.format(name,nodes,properties)
+        """.format(name, nodes, properties)
     tx.run(query)
+
 
 def print_db_indexes(tx):
 
     records = tx.run('SHOW INDEXES')
-    for i,r in enumerate(records):
+    for i, r in enumerate(records):
         if i == 0:
             print("Database Indexes")
         d = dict(r)
         print(" Index Name '{}'".format(d['name']))
         del d['name']
-        s = json.dumps(d,indent=2)
+        s = json.dumps(d, indent=2)
         lines = s.splitlines(keepends=True)
-        print(''.join(['  '+l for l in lines]))
+        print(''.join(['  ' + l for l in lines]))
+
 
 if __name__ == '__main__':
 
@@ -77,7 +82,7 @@ if __name__ == '__main__':
         pw = os.environ['NEO4J_PW']
     except KeyError as e:
         msg = "No environment variable `NEO4J_PW` found.  Consider running export NEO4J_PW='yourpassword' in the current shell environment or in your shell config file."
-        raise KeyError(msg)
+        raise KeyError(msg) from e
 
     driver = GraphDatabase.driver(uri, auth=("neo4j", pw))
 
