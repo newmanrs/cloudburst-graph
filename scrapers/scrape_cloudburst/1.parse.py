b10cad5a9922bded77c3014e56d5e7ace8180613
newmanrs <newmanrs@umich.edu>
2021-11-16 10:13:25 -0800

    formatting cb scraping script

--- scrapers/scrape_cloudburst/1.parse.py
+++ scrapers/scrape_cloudburst/1.parse.py
@@ -4,53 +4,46 @@ import json
 import re
 
 
-with open('tmp/cb.html','r') as f:
+with open('tmp/cb.html', 'r') as f:
     contents = f.read()
-    soup = BeautifulSoup(contents,'html.parser')
+    soup = BeautifulSoup(contents, 'html.parser')
 
 beer_list = []
-beers = soup.find_all('button','cb-beer-button')
+beers = soup.find_all('button', 'cb-beer-button')
 
 for i, beer in enumerate(beers):
 
     # Extract data
     b = dict()
-    b['beer_name'] = beer.find("h3","cb-beer-title").text
+    b['beer_name'] = beer.find("h3", "cb-beer-title").text
 
     # Consider saving the container element for all beers,
     # and not using the raw soup of entire file here - i
     # all file scan in this loop is the bottleneck
-    s = 'cb-beer-modal-'+str(i+1);
-    beer = soup.find('div', id = s)
-    b['beer_style'] = beer.find("span","cb-beer-style").text
+    s = 'cb-beer-modal-'+str(i+1)
+    beer = soup.find('div', id=s)
+    b['beer_style'] = beer.find("span", "cb-beer-style").text
     b['abv'] = beer.find("span", "cb-beer-abv").text
 
-    ibu = beer.find("span","cb-beer-ibu")
+    ibu = beer.find("span", "cb-beer-ibu")
     if ibu is not None:
         b['ibu'] = ibu.text.split(" ")[0]
 
-
-    #Extract description - just be wary format of adding title, ibu, and abv data
+    # Extract description - just be wary format of
+    # adding title, ibu, and abv data
     # is not uniform for all entries.
     des = beer.text
-    des = re.split('\t|\n',des);
-    b['description'] = max(des,key=len)
+    des = re.split('\t|\n', des)
+    b['description'] = max(des, key=len)
 
-
-    #Trim % off abv
+    # Trim % off abv
     b['abv'] = float(b['abv'][0:-1])
     beer_list.append(b)
 
-#with open('hoplist.txt','r') as f:
-#    hops = f.readlines();
-
-#for beer in beer_list:
-#    desc = beer['description'].lower().split(' ');
-#    print(desc)
 
 output_destination = 'tmp/beers.json'
 
-with open(output_destination,'w') as f:
+with open(output_destination, 'w') as f:
     json.dump({'beers': beer_list}, f, indent=2)
 
 print(f"Output written to{output_destination}")
